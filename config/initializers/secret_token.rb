82df00bf0debca809b2f1b5becd1a5b877763ed1
sugan <sugaciitm@gmail.com>
2014-05-21 22:29:06 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Matngo::Application.config.secret_key_base = 'b48047c4cef0923416f1ad4c4a1cc162fc3df9cd0bb1da986516609b83231fea6227b2ba12aac390d7eb930a58dc096127d18e8fbbeee06476c5b5ae71ed0a33'
